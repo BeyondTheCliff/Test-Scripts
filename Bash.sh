9a5ae820c24ee76802807796294e1e68bcd709d6
OSM12345 <ian.smelser@gmail.com>
2015-05-12 16:18:44 -0700

    Commiting

--- Bash.sh
+++ Bash.sh
@@ -1,2 +1,5 @@
-git commit -m "Commiting" .
-git push origin master
+for (( i = 0; i < 5; i++ )); do
+	curl -H "Accept: application/json" -X PUT --data '{"on":false}' http://192.168.0.100/api/newdeveloper/groups/0/action
+	sleep 0.25
+	curl -H "Accept: application/json" -X PUT --data '{"on":true}' http://192.168.0.100/api/newdeveloper/groups/0/action
+done
